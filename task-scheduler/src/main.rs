1081f54e7a876a307b5af3d2a1043f6b49029861
Yaneury Fermin <fermin.yaneury@gmail.com>
2026-07-11 06:48:59 -0700

    basic test

--- task-scheduler/src/main.rs
+++ task-scheduler/src/main.rs
@@ -1,5 +1,29 @@
+use std::time::Duration;
+
 mod schedule;
 
 fn main() {
-    println!("Hello, world!");
+    let mut scheduler = schedule::TaskScheduler::new();
+
+    scheduler.schedule(
+        || println!("Closure A, every 1 sec"),
+        Duration::from_secs(1),
+    );
+
+    scheduler.schedule(
+        || println!("Closure B, every 500ms"),
+        Duration::from_millis(500),
+    );
+
+    scheduler.schedule(
+        || println!("Closure C, every 2 secs"),
+        Duration::from_secs(2),
+    );
+
+    scheduler.schedule(
+        || println!("Closure D, every 100ms"),
+        Duration::from_millis(100),
+    );
+
+    std::thread::sleep(Duration::from_secs(10));
 }
