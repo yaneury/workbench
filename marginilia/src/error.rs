9b8cc62cd59f5566519c2b1c09476821bfca3d0c
Yaneury Fermin <fermin.yaneury@gmail.com>
2026-08-13 18:09:58 -0700

    Add basic error, for SPI atm

---
+++ marginilia/src/error.rs
@@ -0,0 +1,17 @@
+pub enum Error<E> {
+    Spi(E),
+}
+
+impl<E> From<core::convert::Infallible> for Error<E> {
+    fn from(e: core::convert::Infallible) -> Self {
+        match e {}
+    }
+}
+
+impl<E: core::fmt::Display> core::fmt::Display for Error<E> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Error::Spi(e) => write!(f, "SPI error: {e}"),
+        }
+    }
+}
